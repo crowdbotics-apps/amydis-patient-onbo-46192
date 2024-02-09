a28e041021c429c6a4fa8289a454aab4bf1209f2
Crowdbotics <team@crowdbotics.com>
2024-02-09 19:52:13 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "amydis-patient-onbo-46192",
+  url: "https://amydis-patient-onbo-46192.botics.co",
+  api: "https://amydis-patient-onbo-46192.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
